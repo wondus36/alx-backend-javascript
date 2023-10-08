a0eb8f1622a5e9aa968be224c37a5fd7d2517242
wondu shiferaw <33760699+wondus36@users.noreply.github.com>
2023-10-08 16:41:56 +0300

    Add files via upload

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,3 +1,8 @@
 export default function signUpUser(firstName, lastName) {
-  return Promise.resolve({ firstName, lastName });
+  return Promise.resolve(
+    {
+      firstName,
+      lastName,
+    },
+  );
 }
